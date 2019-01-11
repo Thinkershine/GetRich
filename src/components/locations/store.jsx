25dfd2e7f6bba06bba01e7cae00c90c643f544b8
Sebastian Siemaszko <siemaszkosebastian@gmail.com>
2019-01-11 20:59:30 +0100

    Change Store Items For Sale Display

--- src/components/locations/store.jsx
+++ src/components/locations/store.jsx
@@ -8,6 +8,37 @@ class Store extends Component {
       <div>
         <h2>Welcome to Our Store!</h2>
         <p>Here You can get any item you may ever need as gold miner...</p>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Mine Type</th>
+              <th scope="col">Power</th>
+              <th scope="col">Energy Consumption</th>
+              <th scope="col">Value</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.itemsForSale.map(item => (
+              <tr scope="row" key={item._id}>
+                <td>{item.name.toUpperCase()} </td>
+                <td>{item.mineType}</td>
+                <td>{item.miningPower}</td>
+                <td>{item.energyConsumption}</td>
+                <td>${item.value}</td>
+                <td>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => this.props.handlePurchase(item)}
+                  >
+                    BUY
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
         <ul className="list-group">
           {this.props.itemsForSale.map(item => (
             <li
@@ -16,8 +47,8 @@ class Store extends Component {
               style={{ cursor: "pointer" }}
               onClick={() => this.props.handlePurchase(item)}
             >
-              {item.name} : ${item.value}- mine : {item.mineType}; energy cost :
-              {item.energyConsumption}; POW : {item.miningPower}
+              {item.name.toUpperCase()} : ${item.value}- mine : {item.mineType};
+              energy cost :{item.energyConsumption}; POW : {item.miningPower}
             </li>
           ))}
         </ul>
